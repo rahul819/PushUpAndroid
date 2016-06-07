882308512597ad9e78ef1a68985ecd31e8a06f0d
rahul sharma <rahulsharma11103@gmail.com>
2016-06-07 19:30:17 +0530

    now commit

---
+++ app/src/main/java/com/example/wagmob/pushupandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.wagmob.pushupandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void testUpload()
+    {
+
+        int i=1+1;
+    }
+}
